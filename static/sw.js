15951c393c6d1f68af63c1ad29af0262effe950d
Alex Ward <alexward@Alexs-MBP-2.lan>
2024-08-02 13:51:06 -0700

    fix: add header for msal auth

--- static/sw.js
+++ static/sw.js
@@ -10,6 +10,18 @@ const dynamic = new Dynamic();
 const userKey = new URL(location).searchParams.get("userkey");
 self.dynamic = dynamic;
 
+function decode(str) {
+    if (!str) return str;
+    const [input, ...search] = str.split("?");
+    let result = "";
+    const decoded = decodeURIComponent(input);
+    for (let i = 0; i < decoded.length; i++) {
+        result +=
+            i % 2 ? String.fromCharCode(decoded.charCodeAt(i) ^ 2) : decoded[i];
+    }
+    return result + (search.length ? "?" + search.join("?") : "");
+}
+
 self.addEventListener("fetch", event => {
   event.respondWith(
     (async () => {
@@ -18,7 +30,21 @@ self.addEventListener("fetch", event => {
       }
 
       if (event.request.url.startsWith(`${location.origin}/a/`)) {
-        return await uv.fetch(event);
+          const trimmedPath = event.request.url.replace(`${location.origin}/a/`, '');
+          const decodedUrl = decode(trimmedPath);
+          console.error('fetch', event.request.url, decodedUrl);
+          if (decodedUrl && decodedUrl.startsWith('ms-xal-000000004c20a908://auth/')){
+              console.log('Caught Access Token', decodedUrl);
+              return new Response('Please wait...', {
+                  status: 404,
+                  headers: {
+                      'Content-Type': 'text/plain',
+                      'X-MSAL-TOKEN': decodedUrl
+                  }
+              });
+          } else {
+              return await uv.fetch(event);
+          }
       }
 
       return await fetch(event.request);
